5f1b3442d2331eb7e6006be960dcbb07bfae5d91
Ayush Kaneriya <ayushkaneriya05@gmail.com>
2025-09-14 15:21:24 +0530

    fixing start issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "🚀 Starting FastAPI app..."
 
 # Run DB migrations
 echo "📦 Running Alembic migrations..."
-alembic revision --autogenerate -m "Auto migration"
+
 alembic upgrade head
 
 # Start app
